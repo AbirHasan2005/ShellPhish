47375661aa582f38149044246c3c0578fabfed84
Abir Hasan <hackitabirhasan@gmail.com>
2020-07-18 18:20:49 +0600

    Add files via upload

--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@
 # Update Script for shellphish.sh v1.2-beta
 # Script created by @AbirHasan2005
 
-trap 'printf "\n";partial;exit 1' 2
 
 dependencies() {
 
@@ -13,15 +12,16 @@ command -v git > /dev/null 2>&1 || { echo >&2 "Package GIT is not installed ... 
 script() {
 
 clear
-printf "\n \e[1;92mUpdating \e[1;94mshellphish.sh\e[1;92m ...\n\n"
+printf "\n \e[1;92mUpdating \e[1;94mShellPhish\e[1;92m directory ...\n\n"
 sleep 1.5
 cd ..
 rm -rf ShellPhish
 git clone https://github.com/AbirHasan2005/ShellPhish
 cd ShellPhish
 chmod +x shellphish.sh
-printf "\n\e[1;92mRestarting ...\n\e[0m"
+printf "\n\e[1;92m Restarting \e[1;94mshellphish.sh\e[1;92m script ...\n\e[0m"
 bash shellphish.sh
+cd ..
 
 }
 
